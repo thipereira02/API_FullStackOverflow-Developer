810e4fc6228bec3f78aa295befdee38cae457b0c
thipereira02 <thiago.spereira02@gmail.com>
2021-12-13 16:00:54 -0300

    Prepare for deploy

--- src/database.ts
+++ src/database.ts
@@ -5,7 +5,9 @@ const { Pool } = pg;
 
 const connection = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default connection;
