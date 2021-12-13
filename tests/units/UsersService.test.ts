08d3179442eb8397f2b43e1ad6b18668392308c7
thipereira02 <thiago.spereira02@gmail.com>
2021-12-13 15:20:17 -0300

    Adding more units tests for UsersService

--- tests/units/UsersService.test.ts
+++ tests/units/UsersService.test.ts
@@ -16,4 +16,40 @@ describe('Users Service', () => {
     const result = await usersService.create(name, userClass);
     expect(result).toBeTruthy();
   });
+
+  it('Should return falsy when name is empty', async () => {
+    const name = faker.name.firstName();
+    const userClass = 'T1';
+    const token = faker.datatype.uuid();
+
+    jest.spyOn(usersRepository, 'checkName').mockImplementationOnce(async () => false);
+    jest.spyOn(usersRepository, 'createUser').mockImplementationOnce(async () => token);
+
+    const result = await usersService.create(name, userClass);
+    expect(result).toBeFalsy();
+  });
+
+  it('Should return falsy when userClass is empty', async () => {
+    const name = faker.name.firstName();
+    const userClass = '';
+    const token = faker.datatype.uuid();
+
+    jest.spyOn(usersRepository, 'checkName').mockImplementationOnce(async () => false);
+    jest.spyOn(usersRepository, 'createUser').mockImplementationOnce(async () => token);
+
+    const result = await usersService.create(name, userClass);
+    expect(result).toBeFalsy();
+  });
+
+  it('Should return falsy when userClass is invalid', async () => {
+    const name = faker.name.firstName();
+    const userClass = 'T44';
+    const token = faker.datatype.uuid();
+
+    jest.spyOn(usersRepository, 'checkName').mockImplementationOnce(async () => false);
+    jest.spyOn(usersRepository, 'createUser').mockImplementationOnce(async () => token);
+
+    const result = await usersService.create(name, userClass);
+    expect(result).toBeFalsy();
+  });
 });
