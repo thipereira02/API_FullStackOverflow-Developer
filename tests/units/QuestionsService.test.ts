0bbd627c6a2183d82c3e5fe4094cd0279334d813
thipereira02 <thiago.spereira02@gmail.com>
2021-12-13 15:38:16 -0300

    Adding units questionsServices (answerAQuestions) tests

--- tests/units/QuestionsService.test.ts
+++ tests/units/QuestionsService.test.ts
@@ -90,4 +90,40 @@ describe('Questions Service', () => {
     const result = await questionsService.getQuestions();
     expect(result).toBeTruthy();
   });
+
+  it('Should return true when a question is answered', async () => {
+    const answer = faker.random.words();
+    const questionId = Number(faker.datatype.number());
+    const token = faker.datatype.uuid();
+
+    jest.spyOn(questionsRepository, 'checkQuestionId').mockImplementationOnce(async () => true);
+    jest.spyOn(questionsRepository, 'answerQuestion').mockImplementationOnce(async () => true);
+
+    const result = await questionsService.answerAQuestion(answer, questionId, token);
+    expect(result).toBe(true);
+  });
+
+  it('Should return false when an invalid questionId is passed', async () => {
+    const answer = faker.random.words();
+    const questionId = 0;
+    const token = faker.datatype.uuid();
+
+    jest.spyOn(questionsRepository, 'checkQuestionId').mockImplementationOnce(async () => false);
+    jest.spyOn(questionsRepository, 'answerQuestion').mockImplementationOnce(async () => true);
+
+    const result = await questionsService.answerAQuestion(answer, questionId, token);
+    expect(result).toBe(false);
+  });
+
+  it('Should return falsy when answer is empty', async () => {
+    const answer = '';
+    const questionId = Number(faker.datatype.number());
+    const token = faker.datatype.uuid();
+
+    jest.spyOn(questionsRepository, 'checkQuestionId').mockImplementationOnce(async () => true);
+    jest.spyOn(questionsRepository, 'answerQuestion').mockImplementationOnce(async () => false);
+
+    const result = await questionsService.answerAQuestion(answer, questionId, token);
+    expect(result).toBeFalsy();
+  });
 });
